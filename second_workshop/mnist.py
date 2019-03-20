956d8b3e7174b18137b479631fe21b7744501915
Vincent <vincent.lemesle@epitech.eu>
2019-03-20 19:29:17 +0100

    [ADD] -> code to see acuracy for mnist

--- second_workshop/mnist.py
+++ second_workshop/mnist.py
@@ -10,3 +10,9 @@ get_next_images, get_next_outs = mnist.train.next_batch(100)
 get_test_images = mnist.test.images
 # get test labels
 get_test_labels = mnist.test.labels
+
+# true = résultats attendus / pred = prédictions du modèle
+acc, acc_op = tf.metrics.accuracy(labels=tf.argmax(true, axis=1), predictions=tf.argmax(pred, 1))
+accuracy = sess.run(acc_op, feed_dict={true: mnist.test.labels, pred: outs})
+# accuracy = pourcentage de réussite du modèle
+print(accuracy)

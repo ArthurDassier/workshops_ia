a2b0242c2127c193e136056d89e0cb6832744d31
Vincent <vincent.lemesle@epitech.eu>
2019-03-20 19:09:09 +0100

    [CHANGE] -> add outs recuperation for mnist

--- second_workshop/mnist.py
+++ second_workshop/mnist.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # get next 100 images
-get_next_images = mnist.train.next_batch(100)
+get_next_images, get_next_outs = mnist.train.next_batch(100)
 # get test images
 get_test_images = mnist.test.images
 # get test labels

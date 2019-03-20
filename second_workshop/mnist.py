e3b7291b884c0971af89c8a1eb5f398b0bd256b6
Vincent <vincent.lemesle@epitech.eu>
2019-03-20 15:22:56 +0100

    [ADD] -> second workshop exercices

---
+++ second_workshop/mnist.py
@@ -0,0 +1,12 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+# get mnist dataset
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# get next 100 images
+get_next_images = mnist.train.next_batch(100)
+# get test images
+get_test_images = mnist.test.images
+# get test labels
+get_test_labels = mnist.test.labels
